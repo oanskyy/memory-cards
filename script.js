7c139dda0cd7e874cd04edafff76523c1c4d23cd
codessbabe <oana.anghelachi@gmail.com>
2021-03-11 00:05:36 +0100

    Show/hide add-container

--- script.js
+++ script.js
@@ -127,3 +127,6 @@ prevBtn.addEventListener("click", () => {
 })
 
 // Show add container
+showBtn.addEventListener("click", () => addContainer.classList.add('show'))
+// Hide add container
+hideBtn.addEventListener("click", () => addContainer.classList.remove('show'))
